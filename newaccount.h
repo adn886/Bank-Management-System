2d69ae6da2c22d0a419a4302bb4c2fbbd8f05794
Adnan abdullah <38535226+addddd123@users.noreply.github.com>
2018-09-01 16:32:06 +0530

    Update newaccount.h

--- newaccount.h
+++ newaccount.h
@@ -5,47 +5,51 @@ using namespace std;
 #include<stdlib.h>
 #include<stdio.h>
 #include<string.h>
+#include<string>
+#include<conio.h>
 long int updatebalance;
-long int  id()
+int  id()
 {
- char name[15],parentage[10],phone[13],dob;long int money; int i=1;
+ string name,parentage; 
+ char phone[13],dob[8];int money; int i=1;
  ofstream fout;
- cout<<"Enter ur phone no  please write 10 digits";
+ cout<<"Enter ur phone no  please write 10 digits "<<endl;
 cin>>phone;
 int a=strlen(phone);
 if(a!=10)
 {
-	cout<<"PHONe no  incorrect";
+	cout<<"The entered Phone number is incorrect please check the number and try again "<<endl;
 
 	exit(0);
 }
 fout.open(phone);
+cin.ignore();
 
-cout<<"Enter your name\n";
-scanf("%s",name);
+cout<<"Enter your name"<<endl;
+getline(cin,name);
 fout<<"\nNAME= "<<name;
-cout<<"Enter your parentage\n";
-cin>>parentage;
+cout<<"Enter your parentage"<<endl;
+getline(cin,parentage);
 fout<<"\nPARENTAGE=  "<<parentage;
-cout<<"Enter dob\n";
+cout<<"Enter dob"<<endl;
 cin>>dob;
 fout<<"\nDOB=  "<<dob;
-cout<<"if u want to add money to account then any key and if u want to exit then press  1";
+cout<<"if u want to add money to your account then press any key and if u want to exit then press  1 "<<endl;
 scanf("%d",&i);
 if(i!=1)
 {
 
-    cout<<"Plese enter ur ammount in numbers";
+    cout<<"Plese enter ur ammount in numbers"<<endl;
     scanf("%ld",&money);
     
     
     fout<<"\nTOTAL BALANCE= "<<money;
-    cout<<"Your Balance has been updated";
+    cout<<"Your Balance has been updated"<<endl;
     
 }
 
 
-cout<<"moneyyy=="<<money;
+cout<<"Current balance = "<<money;
 return(money);
 fout.close();
 }
