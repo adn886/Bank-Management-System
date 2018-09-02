6ebeebee6459de456e685db9c41d8d053b7f53e7
Adnan abdullah <38535226+addddd123@users.noreply.github.com>
2018-09-02 21:41:25 +0530

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -7,6 +7,7 @@
 #include "newaccount.h"
 #include "addmoney.h"
 #include "display.h"
+#include "delete.h"
 #include<string>
 using namespace std;
 
@@ -17,32 +18,43 @@ long int ak;
 int id();
 void addmoneyy();
 void display();
+void del();
 
 int main()
 {
-int choice;
+int choice,ch;
 cout<<"....Welcome to our Bank and thanks for showing interest....\n........Lets get started........."<<endl;
+do{
+
 cout<<"  ..........MAIN MENU........... "<<endl;
 cout<<"To Select any option press numbers that are displayed before options";
 cout<<"\n 1. Create new account";
 cout<<"\n 2. Update details and balance of your account";
 cout<<"\n 3. Display details of your account";
-cout<<"\n 4. Press to exit";
+cout<<"\n 4. Delete your account";
+cout<<"\n 5. Press to exit\n";
 cout<<"Enter your choice\n";
 cin>>choice;
 switch(choice)
 {
-	
+
  case 1:
     ak =id();
         break;
 case 2:
-       addmoneyy(ak); 
+       addmoneyy(ak);
 	   break;
-case 3: 
+case 3:
        display();
-case 4: exit (0);
+       break;
+case 4: del();
         break;
-default:  cout<<"Enter a valid choice";
+case 5: exit (0);
+        break;
+default:  cout<<"Enter a valid choice"<<endl;
 }
+cout<<endl<<"TO continue press 1 else any other key to exit"<<endl;
+cin>>ch;
+}while(ch==1);
+
 }
