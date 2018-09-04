b7606fc57e33f5e6710ace1f5109ee66fb7ef6f9
Adnan abdullah <38535226+addddd123@users.noreply.github.com>
2018-09-04 16:59:29 +0000

    Update addmoney.h

--- addmoney.h
+++ addmoney.h
@@ -14,10 +14,30 @@ void addmoneyy(long int add)
 	
 	cout<<"u enter wrong no.";
 	exit(0);
-    }
+        }
+//.........date adding.....
+
+     // Declaring argument for time()
+    time_t tt;
+ 
+    // Declaring variable to store return value of
+    // localtime()
+    struct tm * ti;
+
+    // Applying time()
+    time (&tt);
+ 
+    // Using localtime()
+   ti = localtime(&tt);
+   
+//..........ebnding date.........
     cout<<"\n enter amount u want to add\n";
     cin>>k;
     	m=k+add;
+   ob<<"\nDATE OF ADDING MONEY TO ACCOUNT= ";
+//////////////date////////// 
+   ob<< asctime(ti)<<endl;
+///////////////////////////////////   
 	
 	ob<<"\n new ammount u added= "<<k;
 
