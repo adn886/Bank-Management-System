9505aed182d4419067e86dad656e8957de339242
Adnan abdullah <38535226+addddd123@users.noreply.github.com>
2018-09-05 08:01:54 +0000

    Update addmoney.h

--- addmoney.h
+++ addmoney.h
@@ -1,5 +1,6 @@
 #include<fstream>
 #include<iostream>
+#include<string.h>
 using namespace std;
 void addmoneyy(long int add)
 {
@@ -7,14 +8,20 @@ void addmoneyy(long int add)
 	char ch[12];long int n[10]; long int k,m;
 	cout<<"\n please enter your phone no.";
 	cin>>ch;
-	  
+	          
 	ob.open(ch,ios::app);
-	if(!ob)
+	if(!ob||strlen(ch)!=10)
 	{
 	
-	cout<<"u enter wrong no.";
+	cout<<"u enterd wrong no or your account is not present.";
 	exit(0);
         }
+ //.........checking if user wants to deposit or withdraw money....
+      cout<<"\n press D to Deposit \n press W to withdraw";
+       cout<<"\n\n Enter your Choice";
+      char choice;
+      cin>>choice;
+   
 //.........date adding.....
 
      // Declaring argument for time()
@@ -31,7 +38,9 @@ void addmoneyy(long int add)
    ti = localtime(&tt);
    
 //..........ebnding date.........
-    cout<<"\n enter amount u want to add\n";
+   if(choice=='d'||choice=='D')
+{
+    cout<<"\n enter amount you want to Deposit\n";
     cin>>k;
     	m=k+add;
    ob<<"\nDATE OF ADDING MONEY TO ACCOUNT= ";
@@ -42,5 +51,29 @@ void addmoneyy(long int add)
 	ob<<"\n new ammount u added= "<<k;
 
 	ob<<"\n total Balance= "<<m;
-	ob.close();
+//ob.close();
+}
+else
+if(choice=='w'||choice=='W')
+{
+ cout<<"\n enter amount you want to withdraw\n";
+    cin>>k;
+
+ 
+    	m=add-k;
+    if(m<0)
+{
+ cout<<"\n sory you cant withdraw too much ammount";
+      exit(0);
+}   
+   ob<<"\nDATE OF Withdrawing money from your Account ACCOUNT= ";
+//////////////date////////// 
+   ob<< asctime(ti)<<endl;
+///////////////////////////////////   
+	
+	ob<<"\n ammount you withdraw from tour account= "<<k;
+
+	ob<<"\n total Balance= "<<m;
+}
+ob.close();
 }
